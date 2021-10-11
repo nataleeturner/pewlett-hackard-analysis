3200e92a0b4666aebee89bf272bf3c41a995dadc
Natalee Turner <natalee@Natalees-MacBook-Air.local>
2021-10-10 23:31:58 -0500

    Challenge deliverable 2 document upload

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -21,3 +21,15 @@ FROM retiree_titles
 ORDER BY emp_no, to_date DESC;
 
 SELECT * FROM unique_titles;
+
+-- Create a membership eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, ti.title
+INTO membership_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de on (e.emp_no = de.emp_no)
+INNER JOIN titles as ti on (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM membership_eligibility;

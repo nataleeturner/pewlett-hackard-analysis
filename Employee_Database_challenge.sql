01b92e8890e61e0e233fd4bcf5336bd4c57bec66
nataleeturner <88349443+nataleeturner@users.noreply.github.com>
2021-10-11 00:00:34 -0500

    Update challenge doc

---
+++ Employee_Database_challenge.sql
@@ -0,0 +1,44 @@
+-- Challenge deliverable 1
+DROP TABLE retiree_titles;
+
+SELECT e.emp_no, e.first_name, e.last_name, ti.title, ti.from_date, ti.to_date
+INTO retiree_titles
+FROM employees as e
+INNER JOIN titles as ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY emp_no ASC;
+
+SELECT * from retiree_titles;
+
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (emp_no) emp_no,
+first_name,
+last_name,
+title
+INTO unique_titles
+FROM retiree_titles
+ORDER BY emp_no, to_date DESC;
+
+SELECT * FROM unique_titles;
+
+-- Group number of retirees by title
+SELECT COUNT(title), title
+INTO retiring_titles
+FROM unique_titles
+GROUP BY title
+ORDER BY count DESC;
+
+SELECT * FROM retiring_titles;
+
+-- Create a membership eligibility table
+SELECT DISTINCT ON (e.emp_no) e.emp_no, e.first_name, e.last_name, e.birth_date, de.from_date, de.to_date, ti.title
+INTO membership_eligibility
+FROM employees as e
+INNER JOIN dept_emp as de on (e.emp_no = de.emp_no)
+INNER JOIN titles as ti on (e.emp_no = ti.emp_no)
+WHERE (de.to_date = '9999-01-01')
+	AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+ORDER BY emp_no;
+
+SELECT * FROM membership_eligibility;
